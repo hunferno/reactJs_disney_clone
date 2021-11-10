5c2c869ace70b697699ec23ddeb2d533e55712fb
René KONÉ <kone.rene@hotmail.fr>
2021-11-10 09:28:26 +0100

    finalisation du slider avec react-slick

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 function Home() {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
 }
 
 export default Home;
@@ -10,4 +15,17 @@ export default Home;
 const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw - 5px);
+  position: relative;
+
+  &:before {
+    position: absolute;
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: -1;
+  }
 `;
